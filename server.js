6398b3dd25e273f0ee8c2b69f028dcecc508fbde
Rodney Lei Estrada <rodneyestrada2425@gmail.com>
2025-10-06 01:17:07 +0800

    final push

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files from specific directories
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.static(__dirname));
 
 // Serve main page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+// Handle 404s by redirecting to index.html (for SPA behavior)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
     console.log(`Wall-E RL server running at http://localhost:${port}`);
 });
